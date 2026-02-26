e844c6df6ba2931b39ea5aae31202a88fcb785cb
BelizarioJV <joaovictor.fera@hotmail.com>
2026-02-26 15:55:08 -0300

    (update) - aprendendo hooks como useEffect,useRef e useContext

--- commentsSection/src/App.jsx
+++ commentsSection/src/App.jsx
@@ -1,25 +1,35 @@
 import { useState } from "react";
+import { useRef } from "react";
 import "./App.css";
 
 function App() {
+  const textAreaRef = useRef(null);
+  const authorRef = useRef(null);
   const [content, setContent] = useState("");
   const [author, setAuthor] = useState("");
   const [comments, setComments] = useState([]);
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+    if (!author.value && !content.value) {
+      textAreaRef.current.focus();
+      textAreaRef.current.style.backgroundColor = "#f64348";
 
-    const newComment = {
-      id: Math.floor(Math.random() * 1000000), // Gerando um ID único para o comentário
-      author: author,
-      content: content,
-      date: new Date().toLocaleString(),
-    };
-
-    setComments((state) => [newComment, ...state]); // Adicionando o novo comentário à lista de comentários e consestindo o estado anterior
-    setAuthor("");
-    setContent("");
+      authorRef.current.focus();
+      authorRef.current.classList.add("erro"); // outra mandeira de usar o hook ref e sinalizar erro e usando classe e manipulando com css
+    }
   };
+  e.preventDefault();
+
+  const newComment = {
+    id: Math.floor(Math.random() * 1000000), // Gerando um ID único para o comentário
+    author: author,
+    content: content,
+    date: new Date().toLocaleString(),
+  };
+
+  setComments((state) => [newComment, ...state]); // Adicionando o novo comentário à lista de comentários e consestindo o estado anterior
+  setAuthor("");
+  setContent("");
 
   return (
     <>
@@ -30,6 +40,7 @@ function App() {
           <input
             type="email"
             id="author"
+            ref={textAreaRef}
             required
             value={author}
             onChange={(e) => setAuthor(e.target.value)}
@@ -38,6 +49,7 @@ function App() {
           <label htmlFor="content">Comentario :</label>
           <textarea
             id="content"
+            ref={authorRef}
             cols="30"
             rows="5"
             value={content}
